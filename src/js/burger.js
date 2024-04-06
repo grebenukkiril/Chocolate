36b6b8d4b42391f7d417cc97597eaf2a2f4cbd71
Гребенюк Кіріл <grebenukkiril06@gmail.com>
2024-04-06 23:02:41 +0300

    фцвфцв

--- src/js/burger.js
+++ src/js/burger.js
@@ -4,6 +4,10 @@ const burgerMenuClose = document.getElementById('burgerMenuClose');
 
 burgerMenuBtn.addEventListener("click", () => {
     header.classList.add('burger-active');
-    ReportBody.style.overflow
+    document.body.style.overflow = 'hidden';
+});
+burgerMenuClose.addEventListener("click", () => {
+    header.classList.remove('burger-active');
+    document.body.style.overflow = 'scroll';
 });
 
